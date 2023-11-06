fdcca25ca996a380a8831afedb0ed01d0c60725e
Dmytro Kushnir <dmytro.kushnir@globallogic.com>
2023-11-06 19:17:15 +0200

    Routes init
    Co-authored-by: Dmytro Kushnir <dimakush1@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -14,5 +15,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    "sort-keys": ["error", "asc", {"caseSensitive": true, "natural": false, "minKeys": 2}]
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-})
+  build: {
+    rollupOptions: {
+      // Build two separate bundles, one for each app.
+      input: {
+        inbox: path.resolve(__dirname, 'src/inbox/index.html'),
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  plugins: [
+    react(), eslint(),
+  ],
+});
