af4cc41bde755dbf9a31266e8738c80a0a8ac1fc
Dmytro Kushnir <dmytro.kushnir@globallogic.com>
2024-11-06 16:06:01 +0200

    Implement browse router
    Co-authored-by: Dmytro Kushnir <dimakush1@gmail.com>

--- src/apps/home/App.tsx
+++ src/apps/home/App.tsx
@@ -1,5 +1,5 @@
 import {
-  Routes, Route, HashRouter, Navigate,
+  Routes, Route, BrowserRouter, Navigate,
 } from 'react-router-dom';
 
 import WebProgrammingApp from '../web-programming/App.tsx';
@@ -15,52 +15,19 @@ interface AppProps {
     config: ConfigMapping;
 }
 
-// function Layout() {
-//   const location = useLocation();
-//   return (
-//     <div>
-//       <h1>Home page</h1>
-//       <nav>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/about">About</Link>
-//           </li>
-//           <li>
-//             {location.pathname.startsWith('/web-programming') ? (
-//               <Link to="/">Back to Home</Link>
-//             ) : (
-//               <Link to="/web-programming/">Web Programming</Link>
-//             )}
-//           </li>
-//         </ul>
-//       </nav>
-//
-//       <hr />
-//
-//       <Outlet />
-//       <p>
-//         This example demonstrates how to lazily load both route elements and
-//       </p>
-//     </div>
-//   );
-// }
-
-export default function App({ config }: AppProps) { //   HashRouter could be reimplemented with standard BrowserRouter https://github.com/rafgraph/spa-github-pages to properly reload each sub-page
+export default function App({ config }: AppProps) {
   return (
     <ConfigProvider config={config}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           {/*  todo to implement home page, for now just to redirect ot the first app */}
-          <Route path="/" element={<Navigate to={`${config.apps.wp.appPath as AppNames}/`} />} />
+          <Route path="/" element={<Navigate to={`${config.apps.compArch.appPath as AppNames}/`} />} />
           <Route path={`${config.apps.wp.appPath as AppNames}/*`} element={<WebProgrammingApp appName={config.apps.wp.name as AppNames} />} />
           <Route path={`${config.apps.compArch.appPath as AppNames}/*`} element={<ComputerArchitectureApp appName={config.apps.compArch.name as AppNames} />} />
           <Route path="error" element={<Error />} />
           <Route path="*" element={<NoMatch />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </ConfigProvider>
   );
 }
