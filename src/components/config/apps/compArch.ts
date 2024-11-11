943326868de9802b54d8fe547070ac6fb79ed1c6
Dmytro Kushnir <dmytro.kushnir@globallogic.com>
2024-11-11 16:31:55 +0200

    Implement link for instruction cycles
    This reverts commit af4cc41bde755dbf9a31266e8738c80a0a8ac1fc.

--- src/components/config/apps/compArch.ts
+++ src/components/config/apps/compArch.ts
@@ -83,6 +83,12 @@ const compArchConfig: CommonAppMapping = {
       thumbnail: '/images/apps/comp-arch/addressing-modes.png',
       type: 'link',
     },
+    {
+      description: 'Етапи виконання команди.',
+      link: 'https://uk.wikipedia.org/wiki/%D0%A6%D0%B8%D0%BA%D0%BB_%D0%B2%D0%B8%D0%BA%D0%BE%D0%BD%D0%B0%D0%BD%D0%BD%D1%8F_%D1%96%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D1%96%D0%B9',
+      thumbnail: '/images/apps/comp-arch/instruction-cycle.png',
+      type: 'link',
+    },
   ],
   courseWork: {
     description: 'Проєктування CISC комп’ютера',
